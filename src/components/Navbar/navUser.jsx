cea43b6e092e05f105682d05b095e38396db139c
audrey <vaniaaudrey15@gmail.com>
2023-06-12 00:55:04 +0700

    drpdwn btn profile

--- src/components/Navbar/navUser.jsx
+++ src/components/Navbar/navUser.jsx
@@ -1,44 +1,50 @@
-import React, { useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import './Navbar.css';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Dropdown } from 'react-bootstrap';
+
+import "./Navbar.css";
 
 const NavUser = () => {
   const navigate = useNavigate();
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
 
   useEffect(() => {
     const handleHamburgerClick = () => {
-      const hamburgerMenu = document.querySelector('.hamburger-menu');
-      const navbar = document.querySelector('.navbar');
-      const navLinks = document.querySelector('.nav-links');
+      const hamburgerMenu = document.querySelector(".hamburger-menu");
+      const navbar = document.querySelector(".navbar");
+      const navLinks = document.querySelector(".nav-links");
 
-      hamburgerMenu.classList.toggle('active');
-      navbar.classList.toggle('show-menu');
-      navLinks.classList.toggle('show-menu');
+      hamburgerMenu.classList.toggle("active");
+      navbar.classList.toggle("show-menu");
+      navLinks.classList.toggle("show-menu");
     };
 
     const handleProfileButtonClick = () => {
-      navigate('/editprofile');
+      const dropdownMenu = document.querySelector(".profile-dropdown-menu");
     };
-    
 
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
-    const profileButton = document.querySelector('#profile');
+    const hamburgerMenu = document.querySelector(".hamburger-menu");
+    const profileButton = document.querySelector("#profile");
 
     if (hamburgerMenu) {
-      hamburgerMenu.addEventListener('click', handleHamburgerClick);
+      hamburgerMenu.addEventListener("click", handleHamburgerClick);
     }
 
     if (profileButton) {
-      profileButton.addEventListener('click', handleProfileButtonClick);
+      profileButton.addEventListener("click", handleProfileButtonClick);
     }
 
     return () => {
       if (hamburgerMenu) {
-        hamburgerMenu.removeEventListener('click', handleHamburgerClick);
+        hamburgerMenu.removeEventListener("click", handleHamburgerClick);
       }
 
       if (profileButton) {
-        profileButton.removeEventListener('click', handleProfileButtonClick);
+        profileButton.removeEventListener("click", handleProfileButtonClick);
       }
     };
   }, []);
@@ -65,12 +71,24 @@ const NavUser = () => {
         </ul>
 
         <div className="nav-btn">
-          <button id="profile">
-            <img src="src/assets/img/icon-profile.png" width="30" alt="username" />
-            <p>Skilvul - FE 15</p>
-          </button>
-        </div>
+  
+    <Dropdown show={showDropdown} onClick={toggleDropdown}>
+      <div id="profile">
+        <img
+          src="src/assets/img/icon-profile.png"
+          width={30}
+          height={30}
+          alt="username"
+        />
+        <p>Skilvul - FE 15</p>
+      </div>
 
+      <Dropdown.Menu>
+        <Dropdown.Item href="/editprofile">Edit Profile</Dropdown.Item>
+        <Dropdown.Item href="/">Log Out</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+</div>
         <div className="hamburger-menu">
           <span></span>
           <span></span>
