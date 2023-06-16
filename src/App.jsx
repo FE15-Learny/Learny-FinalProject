1e5665948e47ae087613b1a72033f81229a5ddc5
Angelina-1205 <mayzaangelina.2020@student.uny.ac.id>
2023-06-16 15:03:48 +0700

    finishing about

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import Article from "./pages/Article";
 import Homepage from "./pages/Homepage";
-import Footer from "./components/Footer";
-import Articledetails from "./pages/Articledetails";
 import About from "./pages/About";
 
 function App() {
@@ -11,13 +8,9 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/article" element={<Article />} />
-          <Route path="/article/:articleId" element={<Articledetails />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>
-
-      <Footer />
     </>
   );
 }
--- src/components/common/Chip/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./label.css";
-
-function Chip({ label }) {
-  return <p className="chip">{label}</p>;
-}
-
-export default Chip;
