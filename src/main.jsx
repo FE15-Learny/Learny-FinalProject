b5d56e3f32377bccc8cb8fe4d21fb0c86e66f4de
FadlyLugina <fadlylugina211@gmail.com>
2023-06-16 17:23:24 +0700

    rev

--- src/main.jsx
+++ src/main.jsx
@@ -1,48 +1,18 @@
-<<<<<<< HEAD
-// import React from 'react'
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  // </React.StrictMode>
-=======
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-=======
-// import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from "react-redux";
-import App from './App.jsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import store from './redux/store.js'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <BrowserRouter>
-  <Provider store={store}>
-    <App />
+    <Provider store={store}>
+      <App />
     </Provider>
   </BrowserRouter>
   // </React.StrictMode>
->>>>>>> 4605eb74add4e98595f1e9793f09f5a4a5399b0a
->>>>>>> f23548fa8674d7367d84338a1a6bc543f845294a
 );
