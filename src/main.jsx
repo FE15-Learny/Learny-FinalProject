895d272ca3a5cc12183fd3e931282d22ec38673f
audrey <vaniaaudrey15@gmail.com>
2023-06-16 21:35:51 +0700

    add

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import "./index.css";
+import "src\Index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import store from "./redux/store.js";
 
