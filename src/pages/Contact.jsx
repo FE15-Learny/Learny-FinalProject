c23451877df414c04f571306502a931baaaf9c84
Angelina-1205 <mayzaangelina.2020@student.uny.ac.id>
2023-06-16 23:28:07 +0700

    edit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
 import '../style/Contact.css';
-import '../index.css';
+import '../Index.css';
 
 function Contact() {
   const navigate = useNavigate();
